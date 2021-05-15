93eeb9bafef7d21ef18dc908acbb2f8908c742ff
robertswilliams <robertswilliams@comcast.net>
2021-05-15 20:34:26 +0000

    user managemnet part 2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -40,9 +40,9 @@ def login_request(request):
             return redirect('djangoapp:index')
         else:
             context['message'] = "Invalid username or password."
-            return render(request, 'djangoapp:index', context)
+            return render(request, 'djangoapp/index.html', context)
     else:
-        return render(request, 'djangoapp:index', context)
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
